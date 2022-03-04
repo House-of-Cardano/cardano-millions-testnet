db6bd1bbffdc52638d3712e3431baa79f2d3ede1
simonholmes001 <simonholmesabc@gmail.com>
2022-03-04 16:38:37 +0100

    Updated mint.sh to have the same policyID for all CMT, fixes AB#319

--- scripts/mint.sh
+++ scripts/mint.sh
@@ -3,36 +3,6 @@
 # arguments:
 # utxo for funding and collateral
 
-tokenname="434d4c6f74746f546f6b656e"
-tokenamount="1"
-output="2000000"
-
-mkdir -p ./blockchain/policy
-
-# Generate key pairs ->
-
-cardano-cli address key-gen \
-    --verification-key-file ./blockchain/policy/policy.vkey \
-    --signing-key-file ./blockchain/policy/policy.skey
-    
-# Create a policy.script file and fill it with an empty string ->
-
-touch ./blockchain/policy/policy.script && echo "" > ./blockchain/policy/policy.script
-
-# Use the echo command to populate the file ->
-
-echo "{" >> ./blockchain/policy/policy.script 
-echo "  \"keyHash\": \"$(cardano-cli address key-hash --payment-verification-key-file ./blockchain/policy/policy.vkey)\"," >> ./blockchain/policy/policy.script 
-echo "  \"type\": \"sig\"" >> ./blockchain/policy/policy.script 
-echo "}" >> ./blockchain/policy/policy.script
-
-echo "Create policy ID..."
-
-cardano-cli transaction policyid --script-file ./blockchain/policy/policy.script >> ./blockchain/policy/policyID
-
-policyid=$(cat ./blockchain/policy/policyID)
-scriptAddr=$(cat ./blockchain/script.addr)
-
 echo "Build and submit the minting transaction to wallet `address2`..."
 
 cardano-cli transaction build \
@@ -40,7 +10,7 @@ cardano-cli transaction build \
 --$testnet \
 --tx-in $1 \
 --tx-in-collateral $1 \
---tx-out "$bank+$output+$tokenamount $policyid.$tokenname" \
+--tx-out "$bank+$minAdaAmount+$tokenamount $policyid.$tokenname" \
 --change-address $bank \
 --mint="$tokenamount $policyid.$tokenname" \
 --minting-script-file ./blockchain/policy/policy.script \
