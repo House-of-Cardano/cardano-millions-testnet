d5543f81e5f413f3a9daa3fb5d484834e2d7dd49
simonholmes001 <simonholmesabc@gmail.com>
2022-03-07 15:41:26 +0100

    Tidy up repo

--- api-backend/controllers/query-db.js
+++
@@ -1,3 +0,0 @@
-exports.getQuery = (req, res, next) => {
-    
-}
--- api-backend/routes/query-db.js
+++
@@ -1,9 +0,0 @@
-const express = required('express');
-
-const feedQuery = require('../controllers/query-db');
-
-const router = express.Router();
-
-router.get('/querydb', feedQuery.getQuery);
-
-module.exports = router;
